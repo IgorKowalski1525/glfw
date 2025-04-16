9d8bab0b8d037e32dfa2cec2829d2464063e9191
Igor <i.kowalski1525@gmail.com>
2025-04-16 15:02:08 +0200

    Re-added GLFW

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,14 @@
-project "GLFW" 
-	kind "StaticLib"
-	language "C" 
-	buildoptions
-	{
-		"/MT"
-	}
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+    
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
         "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h", 
+        "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
         "src/context.c",
         "src/init.c",
@@ -19,28 +16,30 @@ project "GLFW"
         "src/monitor.c",
         "src/vulkan.c",
         "src/window.c"
-    	}
-	
+    }
+    
 	filter "system:windows"
-        	buildoptions { "/utf-8" }
- 		systemversion "latest"
- 		staticruntime "On"
-        	files
-        	{
-            	"src/win32_init.c",
-            	"src/win32_joystick.c",
-            	"src/win32_monitor.c",
-            	"src/win32_time.c",
-            	"src/win32_thread.c",
-            	"src/win32_window.c",
-            	"src/wgl_context.c",
-            	"src/egl_context.c",
-            	"src/osmesa_context.c"
-        	}
+        buildoptions { "/utf-8" }
+        systemversion "latest"
+        staticruntime "On"
+        
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
 		defines 
 		{ 
-            		"_GLFW_WIN32",
-            		"_CRT_SECURE_NO_WARNINGS"
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
 		}
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
